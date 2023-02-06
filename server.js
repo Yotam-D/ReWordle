7e3d3c08bbf2e02652ba9bfcd0fb4c0cdf9575fa
Yotam-D <d.yotam@gmail.com>
2023-02-07 00:14:28 +0200

    changed main index to be play

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ app.use(bodyParser.json())
         console.log(`listening on port ${process.env.PORT}`);
     })
 
-function setUserInfo(Info) {
-    userInfo = Info;
-    }
+// function setUserInfo(Info) {
+//     userInfo = Info;
+//     }
 
-const db = knex({
-    client:'pg',
-    connection: {
-      host : process.env.DB_HOST,     
-      port: process.env.DB_PORT,              
-      user : process.env.DB_USER,       
-      password : process.env.DB_PASSWORD,          
-      database : process.env.DB_DATABASE,
-    //   ssl: { rejectUnauthorized: false }
-        }
-    })
+// const db = knex({
+//     client:'pg',
+//     connection: {
+//       host : process.env.DB_HOST,     
+//       port: process.env.DB_PORT,              
+//       user : process.env.DB_USER,       
+//       password : process.env.DB_PASSWORD,          
+//       database : process.env.DB_DATABASE,
+//     //   ssl: { rejectUnauthorized: false }
+//         }
+//     })
         
 // send the client side answer wether 'valid user' or 'invalid user' after comparing request username and password with the DB data
 app.post('/login',(req,res) =>{
@@ -51,7 +51,8 @@ app.post('/login',(req,res) =>{
     })
 
 // renders game page after login completed
-app.get('/play', (req,res) =>{
+// app.get('/play', (req,res) =>{
+app.get('/', (req,res) =>{
         res.render('playClassic')
     })
 
